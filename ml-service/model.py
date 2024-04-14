391e2f2a1c764ec810b8b472bbd6a3ec50a0ad7d
Gloria <Gloria@MacBook-di-Gloria.local>
2024-04-14 12:55:54 +0200

    added cuda

--- ml-service/model.py
+++ ml-service/model.py
@@ -1,4 +1,5 @@
-from transformers import AutoModelForSequenceClassification, TFAutoModelForSequenceClassification, AutoTokenize
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
+import torch
 import os
 
 class Model:
@@ -6,25 +7,28 @@ class Model:
         self.model_name = model_name
         self.model = self.load_model()
         self.tokenizer = self.load_tokenizer()
-    
-  
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.model.to(self.device)
     
     def load_model(self):
-        if(not os.path.exists(f"./models/{self.model_name}")):
+        if not os.path.exists(f"./models/{self.model_name}"):
             model = AutoModelForSequenceClassification.from_pretrained(self.model_name)
             model.save_pretrained(f"./models/{self.model_name}")
         else:
             model = AutoModelForSequenceClassification.from_pretrained(f"./models/{self.model_name}")
         return model
+    
     def load_tokenizer(self):
-        if(not os.path.exists(f"./models/{self.model_name}")):
-            tokenizer= AutoTokenize.from_pretrained(self.model_name)
+        if not os.path.exists(f"./models/{self.model_name}"):
+            tokenizer = AutoTokenizer.from_pretrained(self.model_name)
             tokenizer.save_pretrained(f"./models/{self.model_name}")
         else:
-            tokenizer= AutoTokenize.from_pretrained(f"./models/{self.model_name}")
+            tokenizer = AutoTokenizer.from_pretrained(f"./models/{self.model_name}")
         return tokenizer
+    
     def generate(self, text):
-        inputs = self.tokenizer(text, return_tensors="pt")
-        outputs = self.model(**inputs)
+        inputs = self.tokenizer(text, return_tensors="pt", padding=True, truncation=True)
+        inputs = inputs.to(self.device)
+        with torch.no_grad():
+            outputs = self.model(**inputs)
         return outputs
- 
